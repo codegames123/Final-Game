aed65ff98dc99218578ff3d62f9e698869750167
codegames123 <103101101+codegames123@users.noreply.github.com>
2022-05-12 21:53:08 -0700

    added prefabs and music

---
+++ src/prefabs/Collisions.js
@@ -0,0 +1,12 @@
+class Collisions extends Player {
+    constructor(scene, x, y, texture, frame,layer) {
+        super(scene, x, y, texture, frame,layer);
+
+        
+    }
+
+    // preload(){
+
+    // }
+
+}
